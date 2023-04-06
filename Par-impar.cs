a64eb9dec40225880da4a476a4d7dec2265ef52f
Beatriz Aimi <biagioni.aimi@gmail.com>
2023-04-06 10:16:16 -0300

    Add files via upload

---
+++ Par-impar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Exercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int NumeroDigitado;
+
+            Console.Write("Insira um número: ");
+
+            NumeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            if (NumeroDigitado % 2 == 0)
+            {
+                //Número Par
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                //Número Impar
+                Console.WriteLine("Impar");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
